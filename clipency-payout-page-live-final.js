33bbb18a95ca9bf98c77f8b9be78ea18e6ec1186
Smit Bharat Patil <patilsmit2006@gmail.com>
2026-07-01 04:08:41 +0000

    chore: remove 230+ backup files, incoming/, dead scripts and root duplicates

--- clipency-payout-page-live-final.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  window.CLIPENCY_PAYOUT_LIVE_FINAL = "disabled-safe-noop";
-  window.CLIPENCY_PAYOUT_TARGETED_LIVE = "disabled-safe-noop";
-  console.log("[Clipency] destructive payout override disabled safely.");
-})();\n
